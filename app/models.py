95bf61a09e7cafb7ea7d153cb5d4cfdbe06a6b11
santosh <santosh.chaurasia@entercoms.com>
2020-09-09 15:57:33 +0530

    error track

--- app/models.py
+++ app/models.py
@@ -102,7 +102,7 @@ class RAW_STOCK_IN_HAND(TXNBase):
         return {"data":data,"recordsTotal":len(data),"recordsFiltered":len(data)}
     
     def toJson(self):
-        print("available_weight type ",type(self.available_weight))
+        print("available_weight type -",type(self.available_weight))
         return {
             "id": self.id,
             "material" : self.material_name,
